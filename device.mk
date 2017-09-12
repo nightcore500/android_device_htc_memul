903f9ef2443950f78d217c465f6291e998fda2db
nightcore500 <nightcore500@users.noreply.github.com>
2017-09-12 05:21:08 +0200

    gps: Import GPS HAL  * QC package version: LA.BF.1.1.3_rb1.13

--- device.mk
+++ device.mk
@@ -69,6 +69,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
       libshim_log
 
+# GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf
+
 # Camera shim
 PRODUCT_PACKAGES += \
     libshim_camera

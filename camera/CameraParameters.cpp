3e02806d4581f8b0b564e46a85a1e42b511fd2fa
nightcore500 <nightcore500@users.noreply.github.com>
2017-09-12 06:28:34 +0200

    camera: Disable face detection in the params     camera: Disable face detection in the params     In addition to making the UI saner (it will no longer let you     enable / disable face detection when we have already removed     support for it in the lower levels) this should fix manual     focussing the camera when in auto-focus mode.

--- camera/CameraParameters.cpp
+++ camera/CameraParameters.cpp
@@ -183,6 +183,25 @@ const char CameraParameters::CONTI_BURST_CAPTURING[] = "contiburst-capturing";
 const char CameraParameters::CONTI_BURST_CAPTURE_DONE[] = "contiburst-done";
 const char CameraParameters::KEY_FORCE_USE_AUDIO_ENABLED[] = "forceuseaudio";
 
+static String8 get_forced_value(String8 key, String8 value)
+{
+    if (key == "face-detection-values") return String8("off");
+    if (key == "face-detection") return String8("off");
+    return value;
+}
+
+static void add(DefaultKeyedVector<String8,String8> &map, String8 key, String8 value)
+{
+    value = get_forced_value(key, value);
+    map.add(key, value);
+}
+
+static void replaceValueFor(DefaultKeyedVector<String8,String8> &map, String8 key, String8 value)
+{
+    value = get_forced_value(key, value);
+    map.replaceValueFor(key, value);
+}
+
 CameraParameters::CameraParameters()
     : CameraParameters_EXT(this), mMap()
 {
@@ -234,12 +253,12 @@ void CameraParameters::unflatten(const String8 &params)
         if (b == 0) {
             // If there's no semicolon, this is the last item.
             String8 v(a);
-            mMap.add(k, v);
+            add(mMap, k, v);
             break;
         }
 
         String8 v(a, (size_t)(b-a));
-        mMap.add(k, v);
+        add(mMap, k, v);
         a = b+1;
     }
 }
@@ -265,11 +284,11 @@ void CameraParameters::set(const char *key, const char *value)
     // The android SDK only wants one frame, so disable this unless the app
     // explicitly asks for it
     if (!get("hdr-need-1x")) {
-        mMap.replaceValueFor(String8("hdr-need-1x"), String8("false"));
+        replaceValueFor(mMap, String8("hdr-need-1x"), String8("false"));
     }
 #endif
 
-    mMap.replaceValueFor(String8(key), String8(value));
+    replaceValueFor(mMap, String8(key), String8(value));
 }
 
 void CameraParameters::set(const char *key, int value)

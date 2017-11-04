14556f4d52fa8a506e0907e3f22b009ca21d764c
nightcore500 <nightcore500@users.noreply.github.com>
2017-11-04 01:29:45 +0100

    amplifier: Move profile select to audio_amplifier * The TFA amp doesn't really need to know the ins and outs of the audio   system mode definitions.

--- amplifier/tfa9887.c
+++ amplifier/tfa9887.c
@@ -190,21 +190,6 @@ err_disable_i2s:
     return NULL;
 }
 
-static uint32_t get_mode(audio_mode_t mode)
-{
-    switch (mode) {
-        case AUDIO_MODE_RINGTONE:
-            return TFA9887_MODE_RING;
-        case AUDIO_MODE_IN_CALL:
-            return TFA9887_MODE_VOICE;
-        case AUDIO_MODE_IN_COMMUNICATION:
-            return TFA9887_MODE_VOIP;
-        case AUDIO_MODE_NORMAL:
-        default:
-            return TFA9887_MODE_PLAYBACK;
-    }
-}
-
 static int read_file(const char *file_name, uint8_t *buf, int sz, int seek)
 {
     int ret;
@@ -1263,7 +1248,7 @@ priv_init_err:
     return rc;
 }
 
-static int tfa9887_set_dsp_mode(struct tfa9887_amp_t *amp, uint32_t mode)
+static int tfa9887_set_dsp_mode(struct tfa9887_amp_t *amp, int mode)
 {
     int rc;
     const struct mode_config_t *config;
@@ -1488,10 +1473,9 @@ int tfa9887_power(bool on)
     return 0;
 }
 
-int tfa9887_set_mode(audio_mode_t mode)
+int tfa9887_set_mode(int mode)
 {
     int rc, i;
-    uint32_t dsp_mode;
     struct tfa9887_amp_t *amp = NULL;
 
     if (!amps) {
@@ -1499,12 +1483,10 @@ int tfa9887_set_mode(audio_mode_t mode)
         return -ENODEV;
     }
 
-    dsp_mode = get_mode(mode);
-
     for (i = 0; i < AMP_MAX; i++) {
         amp = &amps[i];
-        if (dsp_mode == amp->mode) {
-            ALOGV("%s: No mode change needed, already mode %d", __func__, dsp_mode);
+        if (mode == amp->mode) {
+            ALOGV("%s: No mode change needed, already mode %d", __func__, mode);
             continue;
         }
         rc = tfa9887_lock(amp, true);
@@ -1513,10 +1495,10 @@ int tfa9887_set_mode(audio_mode_t mode)
             continue;
         }
         rc = tfa9887_mute(amp, TFA9887_MUTE_DIGITAL);
-        rc = tfa9887_set_dsp_mode(amp, dsp_mode);
+        rc = tfa9887_set_dsp_mode(amp, mode);
         if (rc == 0) {
             /* Only count DSP mode switches that were successful */
-            amp->mode = dsp_mode;
+            amp->mode = mode;
         }
         rc = tfa9887_mute(amp, TFA9887_MUTE_OFF);
         rc = tfa9887_lock(amp, false);
